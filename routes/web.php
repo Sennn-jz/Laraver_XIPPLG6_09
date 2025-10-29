228a88af45a1db3b359ecafe928b87ae7a3f118a
Sennn-jz <labtopfth@gmail.com>
2025-10-29 16:00:38 +0700

    Add Feature Read Student

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\StudentController;
 
 // Landing Page Route
 Route::get('/', [LandingController::class, 'index'])->name('landing');
 
 // Admin Routes
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index2'])->name('dashboard');
+
+    Route::resource('students', StudentController::class);
 });
